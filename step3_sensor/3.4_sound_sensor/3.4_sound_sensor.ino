4b6156f8f0cae41ae50b5566b0845735cbd47176
のびすけ <rsksound@gmail.com>
2018-03-07 03:25:39 +0900

    update

---
+++ step3_sensor/3.4_sound_sensor/3.4_sound_sensor.ino
@@ -0,0 +1,23 @@
+#define SOUND_SIG A1
+
+void setup() {
+  Serial.begin(115200);                //Start the Serial connection
+  pinMode(SOUND_SIG, INPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(SOUND_SIG);
+  
+  long sum = 0;
+  for(int i=0; i<32; i++){
+    sum += analogRead(SOUND_SIG);
+  }
+  sum >>= 5;
+  if(sum > 1000){
+    Nefry.setLed(255,255,255,0);
+  }else{
+    Nefry.setLed(255,255,255,100);
+  }
+  Serial.println(sum);
+  delay(10);
+}
