41ff753322c20a8c0d4fc4c9691934d1aa916b14
のびすけ <rsksound@gmail.com>
2018-03-13 02:04:30 +0900

    update

--- step4_bi/4.1_ambient/4.1.1_ambient.ino
+++ step4_bi/4.1_ambient/4.1.1_ambient.ino
@@ -1,19 +1,21 @@
 #include <NefryAmbient.h>
 
+#define LIGHT_SIG A1
 NefryAmbient nefryAmbient;
 
 void setup() {
-  Serial.begin(115200);
-  nefryAmbient.begin();//ambientを使えるように初期設定をします。
-  Nefry.setProgramName("AmbientSimpleAnalog");//プログラム名を指定します。
+    Serial.begin(115200);
+    pinMode(LIGHT_SIG, INPUT);
+    nefryAmbient.begin();//ambientを使えるように初期設定をします。
+    Nefry.setProgramName("AmbientSimpleAnalog");//プログラム名を指定します。
 }
 
 void loop() {
-  int analog;
-  analog = analogRead(A1);// A1ピンの状態を取得します。
-  Serial.print("Analog:");
-  Serial.println(analog);//アナログの値を表示します。
-  nefryAmbient.set(1, analog); //field,dataの順にセットします。
-  nefryAmbient.send();//ambientにデータを送信します。
-  delay(1000);//1秒待つ
+    int analog;
+    analog = analogRead(A1);// A1ピンの状態を取得します。
+    Serial.print("Analog:");
+    Serial.println(analog);//アナログの値を表示します。
+    nefryAmbient.set(1, analog); //field,dataの順にセットします。
+    nefryAmbient.send();//ambientにデータを送信します。
+    delay(1000);//1秒待つ
 }
